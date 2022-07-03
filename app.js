215d007e2be18b036b70ebdc46fb92ecc2e3466d
Madeleine Pla <madeleine.pla7@gmail.com>
2022-07-03 15:29:04 -0400

    Prep for heroku

--- app.js
+++ app.js
@@ -1,10 +1,50 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
+const users = require("./routes/api/users")
+const tweets = require("./routes/api/tweets")
+const User = require("./models/User");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const path = require('path');
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
-app.get("/", (req, res) => {
-  res.send("Hello");
-});
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("connected to mongoDB"))
+  .catch(err => console.log(err))
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.use(bodyParser.json());
+
+// app.get("/", (req, res) => {
+//   const user = new User({
+//     handle: "jim",
+//     email: "jim@jim.jim",
+//     password: "jimisgreat123"
+//   })
+//   user.save();
+//   res.send("Hello World");
+// });
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 3000;
 
