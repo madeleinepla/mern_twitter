ed2bf7521657e66062cc742cd2f305264971f7e9
Madeleine Pla <madeleine.pla7@gmail.com>
2022-07-04 09:54:27 -0400

    Fix for heroku

---
+++ frontend/build/precache-manifest.d257189dd6ca9573522e851973e197ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9bbfe79ac06127967e85",
+    "url": "/static/css/main.937ce360.chunk.css"
+  },
+  {
+    "revision": "9bbfe79ac06127967e85",
+    "url": "/static/js/main.9bbfe79a.chunk.js"
+  },
+  {
+    "revision": "5c79cee48b95244d9b89",
+    "url": "/static/js/1.5c79cee4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "291f39d0206ab4b9dd5300cc9b8a2594",
+    "url": "/index.html"
+  }
+];
