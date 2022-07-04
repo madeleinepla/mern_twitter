f0345945cf5b82aff8d716ef7f042744e628ef8c
Madeleine Pla <madeleine.pla7@gmail.com>
2022-07-04 12:48:32 -0400

    Fix for heroku

--- app.js
+++ app.js
@@ -10,14 +10,12 @@ const tweets = require("./routes/api/tweets");
 
 const path = require('path');
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('./frontend/build'));
+  app.use(express.static('frontend/build'));
   app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   })
 }
 
-console.log("hello")
-console.log(process.env.NODE_ENV)
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
